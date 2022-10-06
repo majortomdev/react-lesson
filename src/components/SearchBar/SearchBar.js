5f79e11a10b837d9b975537c6d5b63afcf409669
Joseph Kinahan <kinahanj25@gmail.com>
2022-10-06 22:33:49 +0100

    using function in/of usestate to set state

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,17 +2,21 @@ import React, { useState } from 'react'
 import './SearchBar.css'
 
 const SearchBar = () => {
-    const [searchValue, setSearchValue] = useState("the search value")
+    const [searchValue, setSearchValue] = useState("")
 
     const handleInputChange = (event) => {
         setSearchValue(event.target.value)
+    }
+
+    const handleClearClick = () => {
+        setSearchValue("")
     }
 
 
     return (
     <div>
         <input type='text' value={searchValue} onChange={handleInputChange}/>
-        {searchValue}
+        <button onClick={handleClearClick}>clear</button>
     </div>
     )
 }
